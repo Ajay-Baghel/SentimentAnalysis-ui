6c600b36db247fa51bf9f5f15288fde94d28f036
Ajay Baghel <bajay2926@gmail.com>
2022-06-26 12:14:47 +0530

    added graph to hero section

--- src/components/Bargraph.js
+++ src/components/Bargraph.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect, useRef } from "react";
 import "./Bargraph.css";
 import Papa from "papaparse";
-import { Chart } from "react-chartjs-2";
+import { Bar, Chart } from "react-chartjs-2";
 import { Pie } from "react-chartjs-2";
 import { Chart as ChartJS, registerables } from "chart.js";
+import Button from "./Buttons/Button";
 ChartJS.register(...registerables);
 
 function Bargraph() {
@@ -27,94 +28,106 @@ function Bargraph() {
                     "rgba(255, 26, 104, 0.2)",
                     "rgba(54, 162, 235, 0.2)",
                     "rgba(255, 206, 86, 0.2)",
-                    // "rgba(75, 192, 192, 0.2)",
-                    // "rgba(153, 102, 255, 0.2)",
-                    // "rgba(255, 159, 64, 0.2)",
-                    // "rgba(0, 0, 0, 0.2)",
                 ],
                 borderColor: [
                     "rgba(255, 26, 104, 1)",
                     "rgba(54, 162, 235, 1)",
                     "rgba(255, 206, 86, 1)",
-                    // "rgba(75, 192, 192, 1)",
-                    // "rgba(153, 102, 255, 1)",
-                    // "rgba(255, 159, 64, 1)",
-                    // "rgba(0, 0, 0, 1)",
                 ],
                 borderWidth: 1,
             },
         ],
     };
     function updateChart() {
+        console.log("updatechart() called")
         var positive = positiveData.length,
             negative = negativeData.length,
             neutral = neutralData.length;
         var array = [];
-        // for (let i = 0; i < positiveData.length; i++) {
-        //   if (positiveData[i] == 1) positive++;
-        //   if (negativeData[i] == 1) negative++;
-        //   if (neutralData[i] == 1) neutral++;
-        // }
-        console.log(positive);
-        console.log(negative);
-        console.log(neutral);
         array.push(positive);
         array.push(negative);
         array.push(neutral);
         let barChart = chartRefBar.current;
-        let pieChart = chartRefPie.current;
+        //let pieChart = chartRefPie.current;
         //barChart.data.datasets[0].label = "Reviews";
         barChart.data.datasets[0].data = array;
-        pieChart.data.datasets[0].data = array;
+        //pieChart.data.datasets[0].data = array;
         barChart.update();
-        pieChart.update();
+        console.log("chart updated")
+        //pieChart.update();
     }
     const loadData = () => {
-        if (!isFilePicked) {
-            alert("Please select a file first");
-        } else {
-            Papa.parse(
-                "https://storagetextanalysis.blob.core.windows.net/output-container//home/output/TueJun2107-25-06GMT2022OutputFile.csv",
-                {
-                    download: true,
-                    complete: function (results) {
-                        console.log("results is : ", results);
-                        const sentimentdata = [];
-                        const positivedata = [];
-                        const negativedata = [];
-                        const neutraldata = [];
-                        const res = [[], [], []];
-                        results.data.map((ele) => {
-                            sentimentdata.push(ele.sentiment);
-                            if (ele.sentiment === "positive") {
-                                positivedata.push(ele.positive);
-                                res[0].push(ele.assessment + " " + ele.target);
-                            }
-                            if (ele.sentiment === "negative") {
-                                negativedata.push(ele.negative);
-                                res[1].push(ele.assessment + " " + ele.target);
-                            }
-                            if (ele.sentiment === "mixed") {
-                                neutraldata.push(ele.neutral);
-                                res[2].push(ele.assessment + " " + ele.target);
-                            }
-                        });
-                        console.log("sentiment data : ", sentimentdata);
-                        setRes(res);
-                        setsentimentData(sentimentdata);
-                        setpositiveData(positivedata);
-                        setnegativeData(negativedata);
-                        setneutralData(neutraldata);
-                    },
-                }
-            );
-        }
+        Papa.parse("https://raw.githubusercontent.com/Ajay-Baghel/assets/main/WedJun2211-40-01GMT2022OutputFile.CSV",
+            {
+                download: true,
+                header: true,
+                skipEmptyLines: true,
+                complete: function (results) {
+                    console.log("results is : ", results);
+                    const sentimentdata = [];
+                    const positivedata = [];
+                    const negativedata = [];
+                    const neutraldata = [];
+                    const res = [[], [], []];
+                    results.data.map((ele) => {
+                        sentimentdata.push(ele.sentiment);
+                        if (ele.sentiment === "positive") {
+                            positivedata.push(ele.positive);
+                            res[0].push(ele.assessment + " " + ele.target);
+                        }
+                        if (ele.sentiment === "negative") {
+                            negativedata.push(ele.negative);
+                            res[1].push(ele.assessment + " " + ele.target);
+                        }
+                        if (ele.sentiment === "mixed") {
+                            neutraldata.push(ele.neutral);
+                            res[2].push(ele.assessment + " " + ele.target);
+                        }
+                    });
+                    console.log("sentiment data : ", sentimentdata);
+                    setRes(res);
+                    setsentimentData(sentimentdata);
+                    setpositiveData(positivedata);
+                    setnegativeData(negativedata);
+                    setneutralData(neutraldata);
+                    updateChart();
+                },
+            }
+        )
+
     };
     return (
         <>
             <div className="container">
                 <div className="chart-container">
-                    <Chart
+                    <Bar
+                        data={data}
+                        ref={chartRefBar}
+                        height="600px"
+                        width="800px"
+                        options={{
+                            maintainAspectRatio: false,
+                            plugins: {
+                                legend: {
+                                    display: false,
+                                },
+                                tooltip: {
+                                    callbacks: {
+                                        afterBody: function (context) {
+                                            return res[context[0].dataIndex];
+                                        },
+                                    },
+                                },
+                            },
+                            scales: {
+                                y: {
+                                    beginAtZero: true,
+                                },
+                            },
+                        }}
+
+                    />
+                    {/* <Chart
                         ref={chartRefBar}
                         type="bar"
                         data={data}
@@ -140,8 +153,11 @@ function Bargraph() {
                                 },
                             },
                         }}
-                    />
+                    /> */}
                 </div>
+                <Button onClick={() => { loadData(); updateChart(); }}>Load Data</Button>
+                <Button onClick={() => updateChart()}>update chart</Button>
+
             </div >
         </>
     )
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,21 +1,113 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from "react";
 import './HeroSection.css'
-import { useState } from 'react'
-import Graphs from './Graphs'
-import Bargraph from './Bargraph'
 import Bubblecharts from './Bubblecharts'
 import Button from './Buttons/Button'
+import "./Bargraph.css";
+import Papa from "papaparse";
+import { Bar } from "react-chartjs-2";
+import { Chart as ChartJS, registerables } from "chart.js";
+ChartJS.register(...registerables);
 
 
 const HeroSection = () => {
     const [selectedFile, setSelectedFile] = useState();
     const [isFilePicked, setIsFilePicked] = useState(false);
-
     const changeHandler = (event) => {
         setSelectedFile(event.target.files[0]);
         setIsFilePicked(true);
     };
+    const chartRefBar = useRef(null);
+    const [sentimentData, setsentimentData] = useState([]);
+    const [positiveData, setpositiveData] = useState([]);
+    const [negativeData, setnegativeData] = useState([]);
+    const [neutralData, setneutralData] = useState([]);
+    const [res, setRes] = useState();
 
+    const data = {
+        labels: ["Positive", "Negative", "Neutral"],
+        datasets: [
+            {
+                label: "",
+                data: [],
+                backgroundColor: [
+                    "rgba(255, 26, 104, 0.2)",
+                    "rgba(54, 162, 235, 0.2)",
+                    "rgba(255, 206, 86, 0.2)",
+                ],
+                borderColor: [
+                    "rgba(255, 26, 104, 1)",
+                    "rgba(54, 162, 235, 1)",
+                    "rgba(255, 206, 86, 1)",
+                ],
+                borderWidth: 1,
+            },
+        ],
+    };
+    function updateChart() {
+        console.log("updatechart() called")
+        var positive = positiveData.length,
+            negative = negativeData.length,
+            neutral = neutralData.length;
+        var array = [];
+        array.push(positive);
+        array.push(negative);
+        array.push(neutral);
+        let barChart = chartRefBar.current;
+        barChart.data.datasets[0].data = array;
+        barChart.update();
+        console.log(barChart)
+    }
+    const loadData = () => {
+        Papa.parse("https://raw.githubusercontent.com/Ajay-Baghel/assets/main/WedJun2211-40-01GMT2022OutputFile.CSV",
+            {
+                download: true,
+                header: true,
+                skipEmptyLines: true,
+                complete: function (results) {
+                    console.log("results is : ", results);
+                    const sentimentdata = [];
+                    const positivedata = [];
+                    const negativedata = [];
+                    const neutraldata = [];
+                    const res = [[], [], []];
+                    results.data.map((ele) => {
+                        sentimentdata.push(ele.sentiment);
+                        if (ele.sentiment === "positive") {
+                            positivedata.push(ele.positive);
+                            res[0].push(ele.assessment + " " + ele.target);
+                        }
+                        if (ele.sentiment === "negative") {
+                            negativedata.push(ele.negative);
+                            res[1].push(ele.assessment + " " + ele.target);
+                        }
+                        if (ele.sentiment === "mixed") {
+                            neutraldata.push(ele.neutral);
+                            res[2].push(ele.assessment + " " + ele.target);
+                        }
+                    });
+                    console.log("sentiment data : ", sentimentdata);
+                    setRes(res);
+                    setsentimentData(sentimentdata);
+                    setpositiveData(positivedata);
+                    setnegativeData(negativedata);
+                    setneutralData(neutraldata);
+                    let positive = positiveData.length,
+                        negative = negativeData.length,
+                        neutral = neutralData.length;
+                    let array = [];
+                    console.log(array);
+                    array.push(positive);
+                    array.push(negative);
+                    array.push(neutral);
+                    let barChart = chartRefBar.current;
+                    barChart.data.datasets[0].data = array;
+                    barChart.update();
+                    console.log(barChart)
+                },
+            }
+        )
+
+    };
 
     return (
         <div className="hero-container">
@@ -29,13 +121,47 @@ const HeroSection = () => {
                     id="uploadfile"
                     accept=".csv"
                     onChange={changeHandler}
+                    onClick={loadData}
                 />
                 <div className='hero-btn'>
-                    <Button>GetStarted</Button>
-                    {/* <Button className='btns' buttonStyle='btn--outline'>Get Started</Button> */}
+                    <Button onClick={() => { updateChart() }}>Visualize</Button>
                 </div>
                 <h2>Overall Sentiment of reviews</h2>
-                <Bargraph />
+
+                {/* Bar chart starts here */}
+                <div className="container">
+                    <div className="chart-container">
+                        <Bar
+                            data={data}
+                            ref={chartRefBar}
+                            height="600px"
+                            width="800px"
+                            options={{
+                                maintainAspectRatio: false,
+                                plugins: {
+                                    legend: {
+                                        display: false,
+                                    },
+                                    tooltip: {
+                                        callbacks: {
+                                            afterBody: function (context) {
+                                                return res[context[0].dataIndex];
+                                            },
+                                        },
+                                    },
+                                },
+                                scales: {
+                                    y: {
+                                        beginAtZero: true,
+                                    },
+                                },
+                            }}
+
+                        />
+                    </div>
+
+                </div >
+                {/* Bar chart ends here */}
                 <h2>Sentiment wise assessment of targets</h2>
                 <Bubblecharts />
             </div>
